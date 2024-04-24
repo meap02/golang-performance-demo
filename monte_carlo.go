eaf76907c087125d4c78dd037b599072225539eb
Kyle Just <kjjust7@gmail.com>
2024-04-24 15:32:42 -0700

    Demo Adjustments

--- monte_carlo.go
+++ monte_carlo.go
@@ -40,21 +40,27 @@ func monteCarloPi_run(nodes int, points int) (int, time.Duration) {
 
 // run this function to test the monteCarloPi_run function with multiple nodes and points and output all data to a csv file
 func main() {
-	points := [8]int{100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000}
+	//points := [8]int{100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000}
+	points := [7]int{100, 1000, 10000, 100000, 1000000, 10000000, 100000000}
 
 	nodes := [6]int{2, 4, 8, 16, 32, 64}
 
-	file, err := os.Create("output.csv")
+	file, err := os.Create("go_output.csv")
 
 	if err != nil {
 		log.Fatal("Cannot create file", err)
 	}
 	csvwriter := csv.NewWriter(file)
-	csvwriter.Write([]string{"points", "threads", "pi", "time"})
+	csvwriter.Write([]string{"Points", "Threads", "Pi", "Time"})
 
+	total_time := 0.0
+	total_points := 0
 	for _, nodes := range nodes {
 		for _, points := range points {
 			total, time := monteCarloPi_run(nodes, points)
+			total_time += time.Seconds()
+			total_points += points
+			println(fmt.Sprintf("Go process with %d points and %d threads has finished in %0.8f seconds and a Pi estimation of %0.8f", points, nodes, time.Seconds(), 4*float64(total)/float64(points)))
 			if time.Seconds() == 0 {
 				csvwriter.Write([]string{fmt.Sprintf("%d", points), fmt.Sprintf("%d", nodes), fmt.Sprintf("%0.8f", 4*float64(total)/float64(points)), fmt.Sprintf("0.00000000%0.8d", time.Nanoseconds())})
 			} else {
@@ -63,4 +69,5 @@ func main() {
 			csvwriter.Flush()
 		}
 	}
+	println(fmt.Sprintf("Total time: %0.8f seconds for a total of %d points", total_time, total_points))
 }
